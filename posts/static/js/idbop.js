5f5e1b07f0697b9f699111903b46436913b1fc11
Sol <oezdemircaglayagmur@gmail.com>
2022-10-27 21:21:00 +0300

    final

--- posts/static/js/idbop.js
+++ posts/static/js/idbop.js
@@ -1,9 +1,8 @@
-var dbPromise = idb.open('feeds-db', 5, function(upgradeDb) {
-	upgradeDb.createObjectStore('feeds',{keyPath:'pk'});
-});
-
-
-	//collect latest post from server and store in idb
+	//Open new IndexedDB conncetion.
+	var dbPromise = idb.open('feeds-db', 5, function(upgradeDb) {
+		upgradeDb.createObjectStore('feeds',{keyPath:'pk'});
+	});
+	//collect latest post from server and store in idb.
 	fetch('http://localhost:8000/getdata').then(function(response){
 		return response.json();
 	}).then(function(jsondata){
@@ -17,3 +16,34 @@ var dbPromise = idb.open('feeds-db', 5, function(upgradeDb) {
 	  		}
 		});
 	});
+	//retrive data from idb and display on page.
+	var post="";
+	dbPromise.then(function(db){
+		var tx = db.transaction('feeds', 'readonly');
+  		var feedsStore = tx.objectStore('feeds');
+  		return feedsStore.openCursor();
+	}).then(function logItems(cursor) {
+		  if (!cursor) {
+		  	//if true means we are done cursoring over all records in feeds.
+		  	document.getElementById('offlinedata').innerHTML=post;
+		    return;
+		  }
+		  for (var field in cursor.value) {
+		    	if(field=='fields'){
+		    		feedsData=cursor.value[field];
+		    		for(var key in feedsData){
+		    			if(key =='title'){
+		    				var title = '<h3>'+feedsData[key]+'</h3>';
+		    			}
+		    			if(key =='author'){
+		    				var author = feedsData[key];
+		    			}
+		    			if(key == 'body'){
+		    				var body = '<p>'+feedsData[key]+'</p>';
+		    			}	
+		    		}
+		    		post=post+'<br>'+title+'<br>'+author+'<br>'+body+'<br>';
+		    	}
+		    }
+		  return cursor.continue().then(logItems);
+		});
